e0779c78311d619ec691e6a98bbd9dcd9f67e38e
selphaware <uahmad3013@outlook.com>
2025-02-01 22:22:59 +0000

    add black hole positions

--- space/universe.py
+++ space/universe.py
@@ -33,5 +33,18 @@ class Universe(object):
         return _stars
 
     @property
+    def black_holes(self) -> Dict[str, R2_POS]:
+        _stars: Dict[str, R2_POS] = {}
+        for gname, galaxy in self.universe_positions.items():
+            for sname, system in galaxy['star_systems'].items():
+                if system['is_centre']:
+                    _stars[system['star_name']] = system['origin'][0]
+        return _stars
+
+    @property
     def star_positions(self) -> R2:
         return [x for x in self.stars.values()]
+
+    @property
+    def black_hole_positions(self) -> R2:
+        return [x for x in self.black_holes.values()]
