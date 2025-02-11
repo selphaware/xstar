caf3311fe52597d141cf4dda17bf97e3008330c5
selphaware <uahmad3013@outlook.com>
2025-02-11 21:56:36 +0000

    initialise starship + calc angle between vectors

--- xanimation/xauxi.py
+++ xanimation/xauxi.py
@@ -1,7 +1,32 @@
+import math
+import numpy as np
 from matplotlib.patches import Polygon
 from typing import List, Tuple, Any, Optional
 
 from xanimation.pobject import PhysicalObject
+
+
+def unit_vector(vector):
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between_vectors(
+        v1: List[float],
+        v2: List[float],
+        degree: bool = False
+) -> float:
+    v1 = np.array(v1)
+    v2 = np.array(v2)
+    dotv = np.dot(v1, v2)
+    denom_v1 = np.sqrt(np.dot(v1, v1))
+    denom_v2 = np.sqrt(np.dot(v2, v2))
+    angle = np.arccos(dotv / (denom_v1 * denom_v2))
+
+    if degree:
+        return math.degrees(angle)
+
+    else:
+        return angle
 
 
 def update_all_positions(
@@ -54,3 +79,7 @@ def calc_next_frame_coords(x_vals, xlim, y_vals, ylim):
     if max(x_vals) >= xlim[1]:
         xlim[1] += hori_len
         xlim[0] += hori_len
+
+
+if __name__ == "__main__":
+    print(angle_between_vectors([1, 1], [1, 0]))
